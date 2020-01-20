f486cb4221e6522a3f41c96d2c69f422587f4df4
yarocruz <yarocruz@gmail.com>
2020-01-20 09:09:17 -0500

    added function to reset checkboxes and to check for correct answers

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ let nextBtn = document.querySelector('.next-question');
 let quizStartBtn = document.querySelector('.quiz-start-btn');
 let quizStartContainer = document.querySelector('.quiz-start');
 let quiztContainer = document.querySelector('.quiz-container');
+let feedBack = document.querySelector('.feedback');
 
 // Answer choices Elements
 let answerElement1 = document.querySelector('#answer1');
@@ -17,7 +18,7 @@ const quizQuestions = [
         choice2: "Blocks fill the whole width they are contained in. Inline element don't.",
         choice3: "Blocks are box model elements. Inline are flexbox z-indices.",
         choice4: "Blocks are passed as arguments. Inline elements are API's.",
-        correctAnswer: 2
+        correctAnswer: "2"
     },
     {
         question: "What does Array.flat do?",
@@ -25,7 +26,7 @@ const quizQuestions = [
         choice2: "Blocks fill the whole width they are contained in. Inline element don't.",
         choice3: "A different answer here to show that the looping is working.",
         choice4: "Blocks are passed as arguments. Inline elements are API's.",
-        correctAnswer: 2
+        correctAnswer: "1"
     },
     {
         question: "What are you doing here?",
@@ -33,7 +34,7 @@ const quizQuestions = [
         choice2: "Blocks fill the whole width they are contained in. Inline element don't.",
         choice3: "Blocks are box model elements. Inline are flexbox z-indices.",
         choice4: "A beautiful sample answer right here.",
-        correctAnswer: 2
+        correctAnswer: "4"
     },
     {
         question: "Do you believe in god?",
@@ -41,7 +42,7 @@ const quizQuestions = [
         choice2: "Blocks fill the whole width they are contained in. Inline element don't.",
         choice3: "Blocks are box model elements. Inline are flexbox z-indices.",
         choice4: "Blocks are passed as arguments. Inline elements are API's.",
-        correctAnswer: 2
+        correctAnswer: "2"
     },
     {
         question: "This is another sample question?",
@@ -49,36 +50,48 @@ const quizQuestions = [
         choice2: "This will catch you with your pants down",
         choice3: "Blocks are box model elements. Inline are flexbox z-indices.",
         choice4: "Blocks are passed as arguments. Inline elements are API's.",
-        correctAnswer: 2
+        correctAnswer: "2"
     },
 ]
 
-quizStartBtn.addEventListener('click', function (e) {
-    disableCheckboxes();
-    quizStartContainer.style.display = 'none';
-    quiztContainer.style.display = 'block';
-    questionHeading.innerHTML = quizQuestions[questionIndex].question;
-    answerElement1.innerHTML = quizQuestions[questionIndex].choice1;
-    answerElement2.innerHTML = quizQuestions[questionIndex].choice2;
-    answerElement3.innerHTML = quizQuestions[questionIndex].choice3;
-    answerElement4.innerHTML = quizQuestions[questionIndex].choice4;
-})
-
 let questionIndex = 0;
 
-nextBtn.addEventListener('click', function (e) {
+questionHeading.textContent = quizQuestions[questionIndex].question;
+answerElement1.textContent = quizQuestions[questionIndex].choice1;
+answerElement2.textContent = quizQuestions[questionIndex].choice2;
+answerElement3.textContent = quizQuestions[questionIndex].choice3;
+answerElement4.textContent = quizQuestions[questionIndex].choice4;
+
+quizStartBtn.addEventListener('click', startQuiz)
+
+nextBtn.addEventListener('click', function () {
     if (questionIndex < quizQuestions.length - 1) {
         questionIndex++;
-        questionHeading.innerHTML = quizQuestions[questionIndex].question;
-        answerElement1.innerHTML = quizQuestions[questionIndex].choice1;
-        answerElement2.innerHTML = quizQuestions[questionIndex].choice2;
-        answerElement3.innerHTML = quizQuestions[questionIndex].choice3;
-        answerElement4.innerHTML = quizQuestions[questionIndex].choice4;
+        resetQuestions();
     }
+    questionHeading.textContent = quizQuestions[questionIndex].question;
+    answerElement1.textContent = quizQuestions[questionIndex].choice1;
+    answerElement2.textContent = quizQuestions[questionIndex].choice2;
+    answerElement3.textContent = quizQuestions[questionIndex].choice3;
+    answerElement4.textContent = quizQuestions[questionIndex].choice4;
 })
 
-let checkBoxes = document.querySelectorAll('input[type=checkbox]');
+function startQuiz() {
+    disableCheckboxes();
+    checkAnswers();
+    quizStartContainer.style.display = 'none';
+    quiztContainer.style.display = 'block';
 
+}
+
+function resetQuestions() {
+    for (let checkbox of checkBoxes) {
+        checkbox.checked = false;
+        checkbox.disabled = false;
+    }
+}
+
+let checkBoxes = document.querySelectorAll('input[type=checkbox]');
 
 function disableCheckboxes() {
     for (let checkbox of checkBoxes) {
@@ -95,6 +108,24 @@ function disableCheckboxes() {
         })
     }
 }
+
+function checkAnswers() {
+    for (i = 0; i < checkBoxes.length; i++) {
+        checkBoxes[i].addEventListener('click', (e) => {
+            console.log(e.target.nextSibling.dataset.value);
+            console.log(quizQuestions[questionIndex].correctAnswer);
+            if (e.target.nextSibling.dataset.value === quizQuestions[questionIndex].correctAnswer) {
+                console.log('That is correct');
+            } else {
+                console.log('Answer is wrong');
+            }
+
+        })
+    }
+}
+
+//console.log(checkBoxes[0].nextSibling.textContent);
+
 
 
 
