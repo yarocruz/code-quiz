c1a66ce602c2e5c8af2c9191e27e19d2711413c2
yarocruz <yarocruz@gmail.com>
2020-01-21 20:59:54 -0500

    edited questions array

--- app.js
+++ app.js
@@ -22,40 +22,40 @@ const quizQuestions = [
         question: "What is the difference between a block level element and an inline level element?",
         choice1: "Blocks are big legos. Inline elements are skates",
         choice2: "Blocks fill the whole width they are contained in. Inline element don't.",
-        choice3: "Blocks are box model elements. Inline are flexbox z-indices.",
+        choice3: "Blocks are box model elements. Inline are flexboxes.",
         choice4: "Blocks are passed as arguments. Inline elements are API's.",
         correctAnswer: "2"
     },
     {
         question: "What does Array.flat do?",
         choice1: "It makes an array that has arrays into one array.",
-        choice2: "Blocks fill the whole width they are contained in. Inline element don't.",
-        choice3: "A different answer here to show that the looping is working.",
-        choice4: "Blocks are passed as arguments. Inline elements are API's.",
+        choice2: "It makes fat objects into skinny arrays.",
+        choice3: "It deletes all the items in an array.",
+        choice4: "It makes new indexes in an array.",
         correctAnswer: "1"
     },
     {
-        question: "What are you doing here?",
-        choice1: "These are not the droids you are looking for",
-        choice2: "Blocks fill the whole width they are contained in. Inline element don't.",
-        choice3: "Blocks are box model elements. Inline are flexbox z-indices.",
-        choice4: "A beautiful sample answer right here.",
+        question: "A CSS example of a pseudo selector is...",
+        choice1: ":click",
+        choice2: ":change",
+        choice3: ":hover",
+        choice4: "h1 > p",
+        correctAnswer: "3"
+    },
+    {
+        question: "An example of an HTML element that's self closing",
+        choice1: "The <img> tag",
+        choice2: "The <br> tag",
+        choice3: "The <link> tag",
+        choice4: "All of the above.",
         correctAnswer: "4"
     },
     {
-        question: "Do you believe in god?",
-        choice1: "Blocks are big legos. Inline elements are skates",
-        choice2: "Blocks fill the whole width they are contained in. Inline element don't.",
-        choice3: "Blocks are box model elements. Inline are flexbox z-indices.",
-        choice4: "Blocks are passed as arguments. Inline elements are API's.",
-        correctAnswer: "2"
-    },
-    {
-        question: "This is another sample question?",
-        choice1: "Blocks are big legos. Inline elements are skates",
-        choice2: "This will catch you with your pants down",
-        choice3: "Blocks are box model elements. Inline are flexbox z-indices.",
-        choice4: "Blocks are passed as arguments. Inline elements are API's.",
+        question: "CSS is short for...",
+        choice1: "Cascading Style Sheeps",
+        choice2: "Cascading Style Sheets",
+        choice3: "Centered System Signs",
+        choice4: "Cats Sing Songs",
         correctAnswer: "2"
     },
 ]
