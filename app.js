716fbc9052039d53d30be5206337c0b2582ef49f
yarocruz <yarocruz@gmail.com>
2023-02-19 17:40:00 -0500

    fix some variable declarations and moved the questions into separate file

--- app.js
+++ app.js
@@ -20,49 +20,6 @@ let answerElement2 = document.querySelector('#answer2');
 let answerElement3 = document.querySelector('#answer3');
 let answerElement4 = document.querySelector('#answer4');
 
-const quizQuestions = [
-    {
-        question: "What is the difference between a block level element and an inline level element?",
-        choice1: "Blocks are big legos. Inline elements are skates",
-        choice2: "Blocks fill the whole width they are contained in. Inline element don't.",
-        choice3: "Blocks are box model elements. Inline are flexboxes.",
-        choice4: "Blocks are passed as arguments. Inline elements are API's.",
-        correctAnswer: "2"
-    },
-    {
-        question: "What does Array.flat do?",
-        choice1: "It makes an array that has arrays into one array.",
-        choice2: "It makes fat objects into skinny arrays.",
-        choice3: "It deletes all the items in an array.",
-        choice4: "It makes new indexes in an array.",
-        correctAnswer: "1"
-    },
-    {
-        question: "A CSS example of a pseudo selector is...",
-        choice1: ":click",
-        choice2: ":change",
-        choice3: ":hover",
-        choice4: "h1 > p",
-        correctAnswer: "3"
-    },
-    {
-        question: "An example of an HTML element that's self closing",
-        choice1: "The <img> tag",
-        choice2: "The <br> tag",
-        choice3: "The <link> tag",
-        choice4: "All of the above.",
-        correctAnswer: "4"
-    },
-    {
-        question: "CSS is short for...",
-        choice1: "Cascading Style Sheeps",
-        choice2: "Cascading Style Sheets",
-        choice3: "Centered System Signs",
-        choice4: "Cats Sing Songs",
-        correctAnswer: "2"
-    },
-]
-
 let questionIndex = 0;
 let timeLeft = 60;
 let width = 500;
@@ -82,7 +39,7 @@ function startTimer() {
         if (timeLeft <= 0) {
             stopTimer();
         }
-        for (checkbox of checkBoxes) {
+        for (let checkbox of checkBoxes) {
             if (questionIndex >= quizQuestions.length - 1 && checkbox.checked === true) {
                 stopTimer();
             }
@@ -138,7 +95,7 @@ quizStartBtn.addEventListener('click', startQuiz);
 restartQuizBtn.addEventListener('click', reStartQuiz);
 
 function getNextQuestion() {
-    for (checkbox of checkBoxes) {
+    for (let checkbox of checkBoxes) {
         if (questionIndex < quizQuestions.length - 1 && checkbox.checked) {
             questionIndex++;
             resetQuestions();
---
+++ questions.js
@@ -0,0 +1,42 @@
+const quizQuestions = [
+    {
+        question: "What is the difference between a block level element and an inline level element?",
+        choice1: "Blocks are big legos. Inline elements are skates",
+        choice2: "Blocks fill the whole width they are contained in. Inline element don't.",
+        choice3: "Blocks are box model elements. Inline are flexboxes.",
+        choice4: "Blocks are passed as arguments. Inline elements are API's.",
+        correctAnswer: "2"
+    },
+    {
+        question: "What does Array.flat do?",
+        choice1: "It makes an array that has arrays into one array.",
+        choice2: "It makes fat objects into skinny arrays.",
+        choice3: "It deletes all the items in an array.",
+        choice4: "It makes new indexes in an array.",
+        correctAnswer: "1"
+    },
+    {
+        question: "A CSS example of a pseudo selector is...",
+        choice1: ":click",
+        choice2: ":change",
+        choice3: ":hover",
+        choice4: "h1 > p",
+        correctAnswer: "3"
+    },
+    {
+        question: "An example of an HTML element that's self closing",
+        choice1: "The <img> tag",
+        choice2: "The <br> tag",
+        choice3: "The <link> tag",
+        choice4: "All of the above.",
+        correctAnswer: "4"
+    },
+    {
+        question: "CSS is short for...",
+        choice1: "Cascading Style Sheeps",
+        choice2: "Cascading Style Sheets",
+        choice3: "Centered System Signs",
+        choice4: "Cats Sing Songs",
+        correctAnswer: "2"
+    },
+]
